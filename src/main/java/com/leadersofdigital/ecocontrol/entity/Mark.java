72b9b0fa1a862becaa97e87d0537bec84f2b72cd
Mike Barskiy <mikebarskiy@gmail.com>
2021-11-13 00:03:43 +0300

    Added roles table

--- src/main/java/com/leadersofdigital/ecocontrol/entity/Mark.java
+++ src/main/java/com/leadersofdigital/ecocontrol/entity/Mark.java
@@ -1,6 +1,5 @@
 package com.leadersofdigital.ecocontrol.entity;
 
-import java.io.File;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import javax.persistence.CascadeType;
@@ -9,11 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
-import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,7 +26,7 @@ public class Mark implements Serializable {
   private static final long serialVersionUID = 4099836772284228713L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false)
   private Long id;
 
@@ -39,7 +36,7 @@ public class Mark implements Serializable {
   private String description;
 
   @ManyToOne(cascade = {CascadeType.ALL})
-  private AppUser user;
+  private User user;
 
   @Lob
   private Byte[] img;
---
+++ src/main/java/com/leadersofdigital/ecocontrol/entity/Role.java
@@ -0,0 +1,37 @@
+package com.leadersofdigital.ecocontrol.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "roles")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Transient
+  @ManyToMany(mappedBy = "roles")
+  private Set<User> users;
+}
