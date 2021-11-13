9f7fbbcf43ac55283a9cacc23306fd2622757a71
Regyl <novikovevgeny1@gmail.com>
2021-11-13 16:20:12 +0300

    update dto

--- src/main/java/com/leadersofdigital/ecocontrol/api/controller/dto/response/MarkDtoResponse.java
+++ src/main/java/com/leadersofdigital/ecocontrol/api/controller/dto/response/MarkDtoResponse.java
@@ -5,6 +5,7 @@ import com.leadersofdigital.ecocontrol.entity.Mark;
 import com.leadersofdigital.ecocontrol.entity.enums.PollutionType;
 import lombok.Data;
 
+import java.io.File;
 import java.time.LocalDateTime;
 
 @Data
@@ -14,4 +15,5 @@ public class MarkDtoResponse {
     private PollutionType pollutionType;
     private String description;
     private LocalDateTime createdAt;
+    private File image;
 }
